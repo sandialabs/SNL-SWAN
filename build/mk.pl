abd72106e43b4feeb9f453677a47f0d44e26907d
ccchart <ccchart@sandia.gov>
2014-06-12 15:38:21 -0600

    added building directory

---
+++ build/mk.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use File::Copy;
+
+$os = $^O;
+#print $os;
+
+$arg = $ARGV[0];  # ser, omp, etc.
+
+if ($os =~ /WindowsNT/i || $os =~ /MSWin32/i) {
+  $file1 = "Makefile.win";
+} else {
+  $file1 = "Makefile.unix";
+}
+$file2 = "Makefile";
+
+copy($file1, $file2); # or die "File cannot be copied.";
+unlink(macros.inc); # aka delete
+open($fh,'macros.inc'); # or die "cannot open macros.inc"; # create a blank macros file
+close $fh;
+
+
+if ($os =~ /WindowsNT/i || $os =~ /MSWin32/i) {
+  system("nmake", "config");
+  if ($#ARGV < 0) {
+    system("nmake");
+  } else {
+    system("nmake", $arg);
+  }
+} else {
+  system("make", "config");
+  if ($#ARGV < 0) {
+    system("make");
+  } else {
+    system("make", $arg);
+  }
+}
